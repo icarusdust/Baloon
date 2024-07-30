caf7a01393710c9753d3115c260ea0675888ee0a
Nihal <nihalagazade@gmail.com>
2024-07-30 09:29:35 +0300

    Initial Commit

---
+++ Baloon/BaloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaloonApp.swift
+//  Baloon
+//
+//  Created by nihalagazade on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BaloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
